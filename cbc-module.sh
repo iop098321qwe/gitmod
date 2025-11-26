c1890de9decab1c7b16581fdc460b78d34f4f29a
Dallas Elliott <iop098321qwe@pm.me>
2025-11-26 13:46:49 -0700

    initial commit

---
+++ cbc-module.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# git aliases
+alias gb='git branch'
+alias gco='git checkout $(git branch --all | grep -vE "HEAD|->" | sed -e "s/^[* ]*//" -e "s@remotes/[^/]*/@@g" | fzf --prompt="Branch: ")'
+alias gcom='git checkout main'
+alias gcomm='git commit'
+alias ga='git add'
+alias gaa='git add .'
+alias gpsh='git push'
+alias gpll='git pull'
+alias gpfom='git push --follow-tags origin main'
+alias gs='git status'
+alias gsw='git switch'
+alias gswm='git switch main'
+alias gswt='git switch test'
